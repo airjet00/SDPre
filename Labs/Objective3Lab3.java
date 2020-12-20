e1b7286cf56a2c977be050db2581b7514d0d54e7
erikskell <erikskell@gmail.com>
2020-12-20 11:02:06 -0600

    Obj3Lab3 - random/sum

--- Labs/Objective3Lab3.java
+++ Labs/Objective3Lab3.java
@@ -1,9 +1,10 @@
 public class Objective3Lab3{
   public static void main(String[] args){
+
     int first, second, third, sum;
-    first = 1 + (int)(Math.random()*6);
-    second = 1 + (int)(Math.random()*6);
-    third = 1 + (int)(Math.random()*6);
+    first = 1 + (int)(Math.random()*6);   //sets first random num
+    second = 1 + (int)(Math.random()*6);  //sets second random num
+    third = 1 + (int)(Math.random()*6);   //sets third random num
     sum = first + second + third;
 
     System.out.println(first+" + "+second+" + "+third+" = "+sum);
