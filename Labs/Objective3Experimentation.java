3f7e6e076042a83be40295c8bb5b32261670a387
erikskell <erikskell@gmail.com>
2020-12-20 19:55:53 -0600

    experimentation scanner

--- Labs/Objective3Experimentation.java
+++ Labs/Objective3Experimentation.java
@@ -1,10 +1,17 @@
 public class Objective3Experimentation{
   public static void main(String[] args){
-    int num6 = 10;
-    num6++;
-    System.out.println(num6);
 
-    int num7 = 10;
-    System.out.println(num7);
+    java.util.Scanner scanner = new java.util.Scanner(System.in); //sets scanner to kb
+    int firstNum, secondNum, product;
+
+    System.out.println("Enter first number you want to multiply: ");
+    firstNum = scanner.nextInt();  //takes kb entered and assigns to firstNum
+
+    System.out.println("Enter second number you want to multiply: ");
+    secondNum = scanner.nextInt(); // takes kb entered and assigned to secondNum
+
+    product = firstNum * secondNum;
+
+    System.out.println("The product of " +firstNum+ " and " +secondNum+ " is " + product);
   }
 }
