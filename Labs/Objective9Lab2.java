8f06f578b5c5048d2428cd548a90c608f2f3089e
erikskell <erikskell@gmail.com>
2020-12-27 12:23:36 -0600

    Obj9Lab2 Method in

---
+++ Labs/Objective9Lab2.java
@@ -0,0 +1,13 @@
+public class Objective9Lab2{
+  public static void main(String[] args){
+
+    System.out.println("In main");
+    printMessage();                 // printMessage method called
+    System.out.println("In main");
+
+  }
+  public static void printMessage(){  //start printMessage method
+
+    System.out.println("In method");
+  }
+}
