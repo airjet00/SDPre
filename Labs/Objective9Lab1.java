70fe81c326a697fbf0a0ad6c118534e2fb151f18
erikskell <erikskell@gmail.com>
2020-12-27 11:40:20 -0600

    Obj9Lab1 printName method

---
+++ Labs/Objective9Lab1.java
@@ -0,0 +1,10 @@
+public class Objective9Lab1{
+  public static void main(String[] args){
+
+    printName();        //calls printName method
+
+  }
+  public static void printName(){   //start printName method
+    System.out.println("Hello Doug");
+  }
+}
