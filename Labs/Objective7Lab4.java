fb9d5239340350a65aeb611d75f481aa2e53e35c
erikskell <erikskell@gmail.com>
2020-12-26 20:47:11 -0600

    Obj7Lab4 sum counter

---
+++ Labs/Objective7Lab4.java
@@ -0,0 +1,12 @@
+public class Objective7Lab4{
+  public static void main(String[] args){
+    int counter = 1;
+    int sum = 0;
+
+    while (counter <= 20){      //loop to 20
+      sum += counter;           //add counter amt to sum
+      counter++;
+    }
+    System.out.println(sum);
+  }
+}
